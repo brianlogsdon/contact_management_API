8fa2e2162c326c2a884f7500272cd86515ba13d1
Brian E Logsdon <brianlogsdon2@gmail.com>
2019-01-14 00:07:25 +0000

    update method

--- api/models.py
+++ api/models.py
@@ -3,8 +3,13 @@ from django.db import models
 
 # Create your models here. 
 class Contact(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    full_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    agenda_slug = models.CharField(max_length=50,default='downtown_vi')
+    address = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    
+    
     
 
 class ContactSerializer(serializers.ModelSerializer):
